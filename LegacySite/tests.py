d82becef791c044c65ca92cc1d0a8462040c1478
Kang In Park <kip218@nyu.edu>
2022-03-25 20:21:36 -0400

    push to test cmd injection on my mac setup

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -32,14 +32,24 @@ class MyTest(TestCase):
         response = self.csrfclient.get('/gift.html')
         # will fail if csrf_protect is not used
         self.assertContains(response, 'csrfmiddlewaretoken')
-        response = self.csrfclient.post('/gift.html?username=admin&amount=500')
-        # will fail if csrf is attempted
+        response = self.csrfclient.post('/gift.html', {'username':'attacker', 'amount':1337})
+        # will fail if csrf goes through
         self.assertEqual(response.status_code, 403)
 
-    #incomplete
     def test_sql_injection(self):
-        self.client.post('/register.html?uname=attacker&pword=nefarious&pword2=nefarious')
+        # self.client.post('/register.html', {'uname':'attacker', 'pword':'nefarious', 'pword2':'nefarious'})
+        admin_password_hash = '000000000000000000000000000078d2$18821d89de11ab18488fdc0a01f1ddf4d290e198b0f80cd4974fc031dc2615a3'
         self.client.login(username='attacker', password='nefarious')
         payload = """{"merchant_id": "NYU Apparel Card", "customer_id": "asdf", "total_value": "23", "records": [{"record_type": "amount_change", "amount_added": 2000, "signature": "' AND 1=0 UNION SELECT password FROM LegacySite_user WHERE username = 'admin' -- "}]}"""
         sql_gftcrd = StringIO(payload)
-        
+        response = self.client.post('/use.html', {'card_data':sql_gftcrd, 'card_supplied':True})
+        # will fail if correct admin_password_hash is leaked through sql injection
+        self.assertNotContains(response, admin_password_hash)
+
+    #incomplete
+    def test_command_injection(self):
+        self.client.login(username='attacker', password='nefarious')
+        gftcrd = StringIO("CORRUPTED")
+        payload = "; echo HELLO YOU ARE BEING INJECTED; whoami; ifconfig;"
+        payload = 'attacker'
+        response = self.client.post('/use.html', {'card_data':gftcrd, 'card_supplied':True, 'card_fname':payload})
--- LegacySite/views.py
+++ LegacySite/views.py
@@ -209,7 +209,7 @@ def use_card_view(request):
         print(card_data.strip())
         signature = json.loads(card_data)['records'][0]['signature']
         # signatures should be pretty unique, right?
-        card_query = Card.objects.raw('select id from LegacySite_card where data LIKE \'%%%s%%\'' % signature)
+        card_query = Card.objects.raw('select id from LegacySite_card where data LIKE \'%%%s%%\'', {'signature':signature})
         user_cards = Card.objects.raw('select id, count(*) as count from LegacySite_card where LegacySite_card.user_id = %s' % str(request.user.id))
         card_query_string = ""
         print("Found %s cards" % len(card_query))
