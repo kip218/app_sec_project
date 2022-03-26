8008510edfe5ba22bb9d888b125ef59081898b66
Kang In Park <kip218@nyu.edu>
2022-03-25 23:48:40 -0400

    part2

--- LegacySite/models.py
+++ LegacySite/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.backends import BaseBackend
 from . import extras
+from fernet_fields import EncryptedBinaryField
 
 # Create your models here.
 class User(AbstractBaseUser):
@@ -37,7 +38,8 @@ class Product(models.Model):
 
 class Card(models.Model):
     id = models.AutoField(primary_key=True)
-    data = models.BinaryField(unique=True)
+    # data = models.BinaryField(unique=True)
+    data = EncryptedBinaryField()
     product = models.ForeignKey('LegacySite.Product', on_delete=models.CASCADE, default=None)
     amount = models.IntegerField()
     fp = models.CharField(max_length=100, unique=True)
